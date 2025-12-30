34e1377df4476d44fa3fc41177c3f57c8a7bc57b
JLBMaritime <jonathan.ludford-brooks@jlbmaritime.com>
2025-12-30 18:30:54 +0000

    Add files via upload

--- cli/adsb_cli.py
+++ cli/adsb_cli.py
@@ -10,8 +10,8 @@ import sys
 import os
 import subprocess
 
-# Add parent directory to path
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+# Add parent directory to path (resolve symlinks for global installation)
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 from cli.utils import *
 from cli.wifi_menu import show_wifi_menu
